bd892aa4148cb4511801fe997f5d8b8bd4afccea
yhxu <tracerxu@126.com>
2019-04-05 19:28:50 +0800

    first

---
+++ MyGameScripts/AI/basic class/AILocomotion.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AILocomotion : Vehicle
+{
+	//AI角色的角色控制器
+	private CharacterController controller;
+	//AI角色的Rigidbody
+	private Rigidbody theRigidbody;
+	//AI角色每次的移动距离
+	private Vector3 moveDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //如果有角色控制器，则获得该控制器
+		controller = GetComponent<CharacterController>();
+		//如果有Rigidbody，则获取它
+		theRigidbody = GetComponent<Rigidbody>();
+		//离散时间内需要移动的距离
+		moveDistance = new Vector3(0,0,0);
+		//调用基类的start函数，进行所需初始化
+		base.start();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		//计算速度
+		velocity += acceleration*Time.fixedDeltaTime;
+		//限制速度低于最大速度
+		if (velocity.sqrMagnitude > sqrMaxSpeed)
+		{
+			velocity = velocity.normalized*maxSpeed;
+		}
+		//计算离散时间内移动距离
+		moveDistance = velocity*Time.fixedDeltaTime;
+		//如果是2D游戏，就将z置为0
+		if (isPlanar)
+		{
+			velocity.y = 0;
+			moveDistance.y = 0;
+		}
+
+		//如果AI角色已经添加了角色控制器，则使用控制器移动
+		if (controller != null)
+			controller.SimpleMove(velocity);
+		//如果AI角色没有角色控制器也没有Rigidbody，或者拥有Rigidbody但是要用动力学方式控制运动
+		else if (theRigidbody == null || theRigidbody.isKinematic)
+			transform.position += moveDistance;
+		//用Rigidbody来控制AI角色
+		else
+			theRigidbody.MovePosition(theRigidbody.position + moveDistance);
+		//如果速度大于一个阈值（防止抖动），则更新朝向
+		if (velocity.sqrMagnitude > 0.00001)
+		{
+			//通过当前朝向与速度方向的插值，计算新的朝向
+			Vector3 newForward = newForward;
+		}
+		//播放行走动画
+		gameObject.animation.Play("idle");
+    }
+}
---
+++ MyGameScripts/AI/basic class/Vehicle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+	//AI角色包含的操控列表
+	private Steering[] steerings;
+	//AI角色最高速度
+	public float maxSpeed = 10.0f;
+	//AI角色能被施加的最大力
+	public float maxForce = 100.0f;
+	//最大速度的平方，提前算出，省cpu
+	protected float sqrMaxSpeed;
+	//AI角色mass
+	public float mass = 1.0f;
+	//AI角色velocity
+	public Vector3 velocity;
+	//AI角色the velocity to change the direction
+	public float damping = 0.9f; //f means the num is float,it means double without f
+	//操控力更新间隔，为了达到高帧率，不必要每帧更新
+	public float computeInterval = 0.2f;
+	//判断是否是2D游戏，若是则忽略z值
+	public bool isPlanar = true;
+	//计算得到的操控力
+	private Vector3 steeringForce;
+	//AI角色的加速度
+	protected Vector3 acceleration;
+	//计时器
+	private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		steeringForce = new Vector3(0,0,0);
+		sqrMaxSpeed = maxSpeed*maxSpeed;
+		timer = 0;
+		//获得该AI角色所包含的操控行为列表
+		steerings = GetComponents<Steering>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer += Time.deltaTime;
+		steeringForce = new Vector3(0,0,0);
+		//判断计算操控力的时间是否超过了computeInterval，若是则计算
+		if (timer > computeInterval)
+		{
+			//对操控行为列表中的操控行为力乘以权重并求和
+			foreach (Steering s in steerings)
+			{
+				if (s.enabled)
+				{
+					steeringForce += s.Force()*s.weight;
+				}
+			}
+			//使操控力不大于maxForce
+			steeringForce = Vector3.ClampMagnitude(steeringForce,maxForce);
+			//求出加速度
+			acceleration = steeringForce/mass;
+			//重新计数
+			timer = 0;
+		}
+    }
+}
